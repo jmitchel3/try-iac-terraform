6933de45ee8392aab698e0d2abb4dd34b3f6c92c
Justin Mitchel <8030945+codingforentrepreneurs@users.noreply.github.com>
2023-12-05 17:20:51 +0000

    5 - Terraform Init

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        linode = {
+            source = "linode/linode"
+            # version = "2.10.1"
+        }
+    }
+}
+
+# Configure the Linode Provider
+# provider "linode" {
+#   # token = "..."
+# }
