46c21d5f418147154d974147f8a3e2e17931b141
Justin Mitchel <8030945+codingforentrepreneurs@users.noreply.github.com>
2023-12-05 18:54:01 +0000

    12 - Multiple Instances with the Count Argument

--- main.tf
+++ main.tf
@@ -13,15 +13,17 @@ provider "linode" {
 }
 
 resource "linode_instance" "web" {
-    label = "simple_instance"
+    count = var.linode_web_instance_count
+    label = "try-iac-tf-${count.index}"
     image = "linode/ubuntu22.04"
     region = "us-central"
     type = "g6-nanode-1"
     # authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
+    tags = ["try-iac-web"]
     root_pass = var.linode_instance_pw
 }
 
 output "vm_ssh_string" {
-    value = "ssh root@${linode_instance.web.ip_address}"
+    value = "ssh root@${linode_instance.web[0].ip_address}"
     sensitive = false
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "linode_api_token" {
 variable "linode_instance_pw" {
     sensitive = true
 }
+
+variable "linode_web_instance_count" {
+  default = 2
+  description = "The number of instances for webapp servers to create."
+  type = number
+}
